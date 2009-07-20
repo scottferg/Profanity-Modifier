fd7438cce6788b8e13f30ba9523da15cf4c9bba7
Scott Ferguson <sferguson@quikorder.com>
2009-07-20 13:17:15 -0500

    Setup filter list

--- ProfanityModifier.py
+++ ProfanityModifier.py
@@ -2,15 +2,19 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
-def OnRobotAdded( properties, context ):
-	"""Invoked when the robot has been added"""
-	root_wavelet = context.GetRootWavelet( )
-	root_wavelet.CreateBlip( ).GetDocument( ).SetText( "Sup prudes?" )
-
 def OnBlipSubmitted( properties, context ):
     """Invoked when a new blip has been submitted"""
-    profane_words = [ "shit" ]
-    politically_correct_words = [ "poop" ]
+    profane_words = [ "shit",
+                      "fuck",
+                      "rape",
+                      "damn"
+                    ]
+
+    politically_correct_words = [ "poop",
+                                  "gently caress",
+                                  "surprise sex",
+                                  "gosh darn" 
+                                ]
 
     blip = context.GetBlipById( properties['blipId'] )
     contents = blip.GetDocument( ).GetText( )
@@ -26,6 +30,5 @@ if __name__ == '__main__':
 	    image_url = 'http://profanity-modifier.appspot.com/assets/icon.jpg',
 		version = '1',
 	    profile_url = 'http://profanity-modifier.appspot.com/' )
-    profanity_modifier.RegisterHandler( events.WAVELET_SELF_ADDED, OnRobotAdded )
     profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
     profanity_modifier.Run( )
