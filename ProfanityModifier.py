ced6e90f61c5005e126cd1db97cc1263258a3493
Scott Ferguson <sferguson@quikorder.com>
2009-07-20 08:30:12 -0500

    Respond when a wavelet user creates a new blip

--- ProfanityModifier.py
+++ ProfanityModifier.py
@@ -2,34 +2,25 @@ from waveapi import events
 from waveapi import model
 from waveapi import robot
 
-def OnParticipantsChanged( properties, context ):
-	"""Invoked when any participants have been added/removed"""
-	added = properties['participantsAdded']
-	
-	for p in added:
-		Notify( context )
-
 def OnRobotAdded( properties, context ):
 	"""Invoked when the robot has been added"""
 	root_wavelet = context.GetRootWavelet( )
-	root_wavelet.CreateBlip( ).GetDocument( ).SetText( "I'm alive!" )
+	root_wavelet.CreateBlip( ).GetDocument( ).SetText( "Sup prudes?" )
 
 def OnBlipSubmitted( properties, context ):
 	"""Invoked when a new blip has been submitted"""
-	root_wavelet = context.GetRootWavelet( )
-	root_wavelet.CreateBlip( ).GetDocument( ).SetText( properties.keys( ) )
-	
-
-def Notify( context ):
-	root_wavelet = context.GetRootWavelet( )
-	root_wavelet.CreateBlip( ).GetDocument( ).SetText( "Hi everybody!" )
+    blip = context.GetBlipById( properties['blipId'] )
+    contents = blip.GetDocument( ).GetText( )
+    if '???' in contents:
+        q = '"%s"' % contents.replace( '???', '*' ).replace( '"', ' ' )
+        blip.GetDocument( ).SetText( q )
 
 if __name__ == '__main__':
-	myRobot = robot.Robot( 'ProfanityModifier',
+	profanity_modifier = robot.Robot( 'ProfanityModifier',
 		image_url = 'http://profanity-modifier.appspot.com/assets/icon.jpg',
 		version = '1',
 		profile_url = 'http://profanity-modifier.appspot.com/' )
-	myRobot.RegisterHandler( events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged )
-	myRobot.RegisterHandler( events.WAVELET_SELF_ADDED, OnRobotAdded )
-	myRobot.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
-	myRobot.Run( )
+	profanity_modifier.RegisterHandler( events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged )
+	profanity_modifier.RegisterHandler( events.WAVELET_SELF_ADDED, OnRobotAdded )
+	profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
+	profanity_modifier.Run( )
