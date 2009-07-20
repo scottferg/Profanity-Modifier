5e18515d938073cc7b25385f097c55e87775609e
Scott Ferguson <sferguson@quikorder.com>
2009-07-20 11:47:15 -0500

    Set filtering on new blips

--- ProfanityModifier.py
+++ ProfanityModifier.py
@@ -9,31 +9,12 @@ def OnRobotAdded( properties, context ):
 
 def OnBlipSubmitted( properties, context ):
     """Invoked when a new blip has been submitted"""
-    blip = context.GetBlipById(properties['blipId'])
-    contents = blip.GetDocument().GetText()
+    blip = context.GetBlipById( properties['blipId'] )
+    contents = blip.GetDocument( ).GetText( )
     
     if '???' in contents:
-        q = '"%s"' % contents.replace('???', '*').replace('"', ' ')
-    
-        start = 0
-        res = {}
-        for i in range(6):
-            url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=%d&q=%s' % (start, urllib.quote(q))
-            js = urlfetch.fetch(url=url).content
-        
-            for fragment in simplejson.loads(js)['responseData']['results']:
-                for m in re.findall('\<b\>([^\<]*)', fragment['content']):
-                    m = m.lower()
-                    
-                    if m == '...':
-                        continue
-                    res[m] = res.get(m, 0) + 1
-            start += 5
-    
-        if res:
-            res = res.items()
-            res.sort(lambda a,b: -cmp(a[1], b[1]))
-            blip.GetDocument().SetText(res[0][0])
+        q = '"%s"' % contents.replace( '???', '*' )
+        blip.GetDocument().SetText( q )
 
 if __name__ == '__main__':
     profanity_modifier = robot.Robot( 'ProfanityModifier',
