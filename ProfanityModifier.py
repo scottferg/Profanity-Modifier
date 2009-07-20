917486157cdb9becddb0a8c54c38fbe261088b97
Scott Ferguson <sferguson@quikorder.com>
2009-07-20 11:44:31 -0500

    Update version and add callback for adding blips

--- ProfanityModifier.py
+++ ProfanityModifier.py
@@ -8,19 +8,38 @@ def OnRobotAdded( properties, context ):
 	root_wavelet.CreateBlip( ).GetDocument( ).SetText( "Sup prudes?" )
 
 def OnBlipSubmitted( properties, context ):
-	"""Invoked when a new blip has been submitted"""
-    blip = context.GetBlipById( properties['blipId'] )
-    contents = blip.GetDocument( ).GetText( )
+    """Invoked when a new blip has been submitted"""
+    blip = context.GetBlipById(properties['blipId'])
+    contents = blip.GetDocument().GetText()
+    
     if '???' in contents:
-        q = '"%s"' % contents.replace( '???', '*' ).replace( '"', ' ' )
-        blip.GetDocument( ).SetText( q )
+        q = '"%s"' % contents.replace('???', '*').replace('"', ' ')
+    
+        start = 0
+        res = {}
+        for i in range(6):
+            url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=%d&q=%s' % (start, urllib.quote(q))
+            js = urlfetch.fetch(url=url).content
+        
+            for fragment in simplejson.loads(js)['responseData']['results']:
+                for m in re.findall('\<b\>([^\<]*)', fragment['content']):
+                    m = m.lower()
+                    
+                    if m == '...':
+                        continue
+                    res[m] = res.get(m, 0) + 1
+            start += 5
+    
+        if res:
+            res = res.items()
+            res.sort(lambda a,b: -cmp(a[1], b[1]))
+            blip.GetDocument().SetText(res[0][0])
 
 if __name__ == '__main__':
-	profanity_modifier = robot.Robot( 'ProfanityModifier',
-		image_url = 'http://profanity-modifier.appspot.com/assets/icon.jpg',
+    profanity_modifier = robot.Robot( 'ProfanityModifier',
+	    image_url = 'http://profanity-modifier.appspot.com/assets/icon.jpg',
 		version = '1',
-		profile_url = 'http://profanity-modifier.appspot.com/' )
-	profanity_modifier.RegisterHandler( events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged )
-	profanity_modifier.RegisterHandler( events.WAVELET_SELF_ADDED, OnRobotAdded )
-	profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
-	profanity_modifier.Run( )
+	    profile_url = 'http://profanity-modifier.appspot.com/' )
+    profanity_modifier.RegisterHandler( events.WAVELET_SELF_ADDED, OnRobotAdded )
+    profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
+    profanity_modifier.Run( )
