d1b50c89061add24f581e16b17844a1d5b9af6d8
Scott Ferguson <scottwferg@gmail.com>
2009-07-20 23:11:15 -0500

    Add callback for WAVELET_BLIP_CREATED

--- ProfanityModifier.py
+++ ProfanityModifier.py
@@ -3,32 +3,41 @@ from waveapi import model
 from waveapi import robot
 
 def OnBlipSubmitted( properties, context ):
-    """Invoked when a new blip has been submitted"""
-    profane_words = [ "shit",
-                      "fuck",
-                      "rape",
-                      "damn"
-                    ]
+	"""Invoked when a new blip has been submitted"""
+	blip = context.GetBlipById( properties['blipId'] )
+	ReplaceProfanity( blip )
 
-    politically_correct_words = [ "poop",
-                                  "gently caress",
-                                  "surprise sex",
-                                  "gosh darn" 
+def OnWaveletBlipCreated( properties, context ):
+	"""Invoked when a new wavelet has been submitted"""
+	blip = context.GetBlipById( properties['blipId'] )
+	ReplaceProfanity( blip )
+
+def ReplaceProfanity( blip ):
+	profane_words = [ "shit",
+					  "fuck",
+					  "rape",
+					  "damn"
+					]
+
+	politically_correct_words = [ "poop",
+								  "gently caress",
+								  "surprise sex",
+								  "gosh darn" 
                                 ]
 
-    blip = context.GetBlipById( properties['blipId'] )
-    contents = blip.GetDocument( ).GetText( )
+	contents = blip.GetDocument( ).GetText( )
 
-    for word in profane_words:
-        if word in contents:
-            """Replace each profane word with it's politically correct alternative"""
-            q = '"%s"' % contents.replace( word, politically_correct_words[ profane_words.index( word ) ] )
-            blip.GetDocument().SetText( q )
+	for word in profane_words:
+		if word in contents:
+			"""Replace each profane word with it's politically correct alternative"""
+			q = '%s' % contents.replace( word, politically_correct_words[ profane_words.index( word ) ] )
+			blip.GetDocument().SetText( q )
 
 if __name__ == '__main__':
-    profanity_modifier = robot.Robot( 'ProfanityModifier',
+	profanity_modifier = robot.Robot( 'ProfanityModifier',
 	    image_url = 'http://profanity-modifier.appspot.com/assets/icon.jpg',
 		version = '1',
 	    profile_url = 'http://profanity-modifier.appspot.com/' )
-    profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
-    profanity_modifier.Run( )
+	profanity_modifier.RegisterHandler( events.BLIP_SUBMITTED, OnBlipSubmitted )
+	profanity_modifier.RegisterHandler( events.WAVELET_BLIP_CREATED, OnWaveletBlipCreated )
+	profanity_modifier.Run( )
